08a0c8a2559dbbb8709a71ad9f1c36347374b200
Azat Zinnyurov <natau4@gmail.com>
2013-12-14 20:07:03 +0400

    Functionalyty for arrow and zero mark action was added

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,55 @@
 $(function() {
-    var app = app || {
-        Models: {},
-        Collections: {},
-        Views: {}
-    };
+    var app = app || {};
+
+    app.getAngleForCent = function(cent) {
+        return cent + 90;
+    }
+
+    app.Arrow = {
+        id: '#arrow',
+
+        setCent: function(cent) {
+            var angle = app.getAngleForCent(cent);
+            $(this.id).css('-webkit-transform', 'rotate(' + angle + 'deg)');
+        }
+    }
+
+    app.ScaleZeroMark = {
+        id: '#zero',
+
+        isPositive: function(cent)
+        {
+            if (cent >= -5 && cent <= 5) {
+                return true;
+            }
+
+            return false;
+        },
+
+        setCent: function(cent) {
+            if (this.isPositive(cent)) {
+                $(this.id).addClass('active');
+            } else {
+                $(this.id).removeClass('active');
+            }
+        }
+    }
+
+    var i = -50;
+    var to = 'right';
+    setInterval(function() {
+        if (to == 'right') {
+            i++;
+        } else if (to == 'left') {
+            i--;
+        }
+        if (i < -50) {
+            to = 'right';
+        }
+        if (i > 50) {
+            to = 'left';
+        }
+        app.Arrow.setCent(i);
+        app.ScaleZeroMark.setCent(i);
+    }, 50);
 });
