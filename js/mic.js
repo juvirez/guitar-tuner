741601daee159fcd1b65497f4b620dd39014d582
juvirez <juvirez@gmail.com>
2013-12-19 01:56:10 +0400

    Increase accuracy x2 (1.3Hz) by storage last buffer

--- js/mic.js
+++ js/mic.js
@@ -15,13 +15,17 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 app.mic = {
 	bufferSize: 16384,
 	sampleRate: 44100,
-	coef: 44100 / 16384,
+	coef: 44100 / (16384*2),
 	fft: null,
+	callback: null,
+	lastBuf: null,
 
 	startAudition: function(callback) {
+		var that = app.mic;
+		that.lastBuff = new Float32Array(that.bufferSize);
+		that.callback = callback;
 		navigator.getMedia({audio: true}, function(stream) {
-			var that = app.mic;
-			that.fft = new FFT(that.bufferSize, that.sampleRate);
+			that.fft = new FFT(2 * that.bufferSize, that.sampleRate);
 			var audioContext = new AudioContext();
 			var audioSource = audioContext.createMediaStreamSource(stream);
 			var tuner = audioContext.createJavaScriptNode(that.bufferSize, 1, 1);
@@ -35,19 +39,28 @@ app.mic = {
 
 	audioProcess: function(e) {
 		var that = app.mic;
-		that.fft.forward(e.inputBuffer.getChannelData(0));
+		var data = e.inputBuffer.getChannelData(0);
+		that.fft.forward(Float32Concat(that.lastBuff, data));
 		var freqs = that.fft.spectrum;
-		var maxI = 0, maxVal = -100;
-		for (var i = 0; i < freqs.length; i++) {
+		var maxI = 0, maxVal = freqs[maxI];
+		for (var i = 1; i < freqs.length; i++) {
 			if (maxVal < freqs[i]) {
 				maxVal = freqs[i];
 				maxI = i;
 			}
 		}
+		that.lastBuff = data;
 		console.log(maxI * that.coef, maxVal);
 	}
 };
 
+function Float32Concat(first, second) {
+    var firstLength = first.length;
+    var result = new Float32Array(firstLength + second.length);
+    result.set(first);
+    result.set(second, firstLength);
+    return result;
+}
 
 $(function() {
 	app.mic.startAudition();
